f3dbeb24d37f1ac8bb39eea11d1d669667915278
gauravrsawant <gauravrsawant1313@gmail.com>
2025-07-06 00:02:48 +0530

    Updated

--- src/config.py
+++ src/config.py
@@ -2,6 +2,6 @@ TRAIN_DIR = "data/UCSD_Anomaly_Dataset.v1p2/UCSDped1/Test"
 TEST_DIR = "data/UCSD_Anomaly_Dataset.v1p2/UCSDped1/Train"
 IMG_SIZE = 224
 BATCH_SIZE = 8
-EPOCHS = 2
+EPOCHS = 10
 MODEL_PATH = "model/model.hdf5"
 FRAMES_PER_CLIP = 10
