02bd18164e29b90a752211b8b642fca7b7947d14
gauravrsawant <gauravrsawant1313@gmail.com>
2025-07-05 20:43:05 +0530

    add configuration file for directory paths and constants

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# scripts/config.py
+
+TRAIN_DIR = "/kaggle/input/ucsd-anomaly-dataset/UCSD_Anomaly_Dataset.v1p2/UCSDped1/Train"
+TEST_DIR = "/kaggle/input/ucsd-anomaly-dataset/UCSD_Anomaly_Dataset.v1p2/UCSDped1/Test"
+IMG_SIZE = 224
+BATCH_SIZE = 8
+EPOCHS = 2
+MODEL_PATH = "model/model.hdf5"
+FRAMES_PER_CLIP = 10
